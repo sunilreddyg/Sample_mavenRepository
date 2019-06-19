d0c049048142dbf67547899418cfe0bad7708578
Administrator <Administrator@192.168.2.2>
2019-06-19 10:43:19 +0530

    New upload

---
+++ Ebanking/src/test/java/p1/Suite1.java
@@ -0,0 +1,24 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class Suite1 {
+  @Test
+  public void tc001()
+  {
+	  System.out.println("tc001 executed");
+  }
+  
+  @Test
+  public void tc002()
+  {
+	  System.out.println("tc001 executed");
+  }
+  
+  
+  @Test
+  public void tc003()
+  {
+	  System.out.println("tc001 executed");
+  }
+}
---
+++ Ebanking/src/test/java/p2/Suite2.java
@@ -0,0 +1,24 @@
+package p2;
+
+import org.testng.annotations.Test;
+
+public class Suite2 {
+  @Test
+  public void tc004()
+  {
+	  System.out.println("tc001 executed");
+  }
+  
+  @Test
+  public void tc005()
+  {
+	  System.out.println("tc001 executed");
+  }
+  
+  
+  @Test
+  public void tc006()
+  {
+	  System.out.println("tc001 executed");
+  }
+}
