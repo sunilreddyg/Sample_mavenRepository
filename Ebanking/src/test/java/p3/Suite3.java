82eeb446c4e91e9fba8639524ebf35bc3541b28d
Administrator <Administrator@192.168.2.2>
2019-06-21 10:45:05 +0530

    updation4

--- Ebanking/src/test/java/p3/Suite3.java
+++ Ebanking/src/test/java/p3/Suite3.java
@@ -4,8 +4,14 @@ import org.testng.annotations.Test;
 
 public class Suite3 {
   @Test
-  public void tc001() 
+  public void tc008() 
   {
 	  System.out.println("tc008 executed");
+  }
+  
+  @Test
+  public void tc009() 
+  {
+	  System.out.println("tc009 executed");
   }
 }
